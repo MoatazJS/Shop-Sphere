d9916e1c08a132d86d170c97e86ea57dc3dcdd0c
Moataz <moataz_abdullah@outlook.com>
2025-09-24 17:30:19 +0300

    added continue shopping button in ordersuccess page

--- src/app/(pages)/order-success/page.tsx
+++ src/app/(pages)/order-success/page.tsx
@@ -1,5 +1,8 @@
 // app/order-success/page.tsx
 
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function OrderSuccessPage({
   searchParams,
 }: {
@@ -10,26 +13,34 @@ export default function OrderSuccessPage({
   const price = searchParams.price;
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-6">
-      <div className="bg-white shadow-lg rounded-2xl p-8 max-w-md w-full text-center">
-        <h1 className="text-2xl font-bold text-green-600 mb-4">
-          🎉 Order Completed Successfully!
-        </h1>
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-6">
+        <div className="bg-white shadow-lg rounded-2xl p-8 max-w-md w-full text-center">
+          <h1 className="text-2xl font-bold text-green-600 mb-4">
+            🎉 Order Completed Successfully!
+          </h1>
 
-        <div className="space-y-2 text-gray-700">
-          <p>
-            <span className="font-semibold">Order ID:</span> {orderId ?? "N/A"}
-          </p>
-          <p>
-            <span className="font-semibold">Delivery Address:</span>{" "}
-            {address ?? "Not provided"}
-          </p>
-          <p>
-            <span className="font-semibold">Total Price:</span>{" "}
-            {price ? `$${price}` : "Not available"}
-          </p>
+          <div className="space-y-2 text-gray-700">
+            <p>
+              <span className="font-semibold">Order ID:</span>{" "}
+              {orderId ?? "N/A"}
+            </p>
+            <p>
+              <span className="font-semibold">Delivery Address:</span>{" "}
+              {address ?? "Not provided"}
+            </p>
+            <p>
+              <span className="font-semibold">Total Price:</span>{" "}
+              {price ? `$${price}` : "Not available"}
+            </p>
+          </div>
         </div>
+        <Link className="mt-2 mx-auto" href={"/products"}>
+          <Button className="bg-orange-500 hover:bg-orange-600 text-gray-200">
+            Continue Shopping
+          </Button>
+        </Link>
       </div>
-    </div>
+    </>
   );
 }
